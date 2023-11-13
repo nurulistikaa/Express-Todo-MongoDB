62424dc86c86b507f6ca5f3b121b454f3b2e2813
Nurul Istika <nurulistika92@gmail.com>
2023-11-13 21:56:03 +0700

    selesai membuat edit todo, delete todo dan delete all todo

--- controllers/todo-controller.js
+++ controllers/todo-controller.js
@@ -15,6 +15,7 @@ module.exports = {
       });
     }
   },
+
   getTodoById: async (req, res) => {
     try {
       const id = req.params.id;
@@ -27,6 +28,7 @@ module.exports = {
       res.status(400).json({ error: error.message });
     }
   },
+
   createTodo: async (req, res) => {
     try {
       let data = req.body;
@@ -36,6 +38,54 @@ module.exports = {
       });
     } catch (error) {
         res.status(400).json({ error: error.message });
+    }
+  },
+
+  editTodo: async (req, res) => {
+    try {
+        const todo = await Todo.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (todo) {
+            res.status(200).json({
+                message: "Berhasil mengedit data todo",
+                data: todo
+              });
+        } else {
+            res.status(404).json({ message: 'Todo not found' });
+        }
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+  },
+
+  deleteTodo: async (req, res) => {
+    try {
+        const todo = await Todo.findByIdAndDelete(req.params.id);
+        if (todo) {
+            res.status(200).json({
+                message: "Berhasil menghapus data todo by id",
+                data: todo
+              });
+        } else {
+            res.status(404).json({ message: 'Todo not found' });
+        }
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+  },
+  
+  deleteAllTodo: async (req, res) => {
+    try {
+        const todo = await Todo.deleteMany();
+        if (todo.deletedCount > 0) {
+            res.status(200).json({
+                message: "Berhasil menghapus semua data todo",
+                data: todo
+              });
+        } else {
+            res.status(404).json({ message: 'No todos to delete' });
+        }
+    } catch (err) {
+        res.status(500).json({ message: err.message });
     }
   },
   
--- routes/todo-route.js
+++ routes/todo-route.js
@@ -4,11 +4,18 @@ const route = express.Router();
 const {
   getAllTodo,
   getTodoById,
-  createTodo
+  createTodo,
+  editTodo,
+  deleteTodo,
+  deleteAllTodo
 } = require("../controllers/todo-controller");
+const verifyToken = require("../middleware/auth");
 
 route.get("/", getAllTodo);
 route.get("/:id", getTodoById);
 route.post("/", createTodo);
+route.put("/:id", verifyToken, editTodo);
+route.delete("/:id", verifyToken, deleteTodo);
+route.delete("/", deleteAllTodo);
 
 module.exports = route;
